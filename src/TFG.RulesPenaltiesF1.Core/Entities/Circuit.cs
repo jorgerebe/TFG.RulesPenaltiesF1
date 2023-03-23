c0530503264ecfd6e072f94d76540f2b49cccd04
Jorge Rebé <jorge.rebe@estudiantes.uva.es>
2023-03-23 21:37:22 +0100

    inicio hu09 - países y circuitos

---
+++ src/TFG.RulesPenaltiesF1.Core/Entities/Circuit.cs
@@ -0,0 +1,43 @@
+﻿using TFG.RulesPenaltiesF1.Core.Interfaces;
+
+namespace TFG.RulesPenaltiesF1.Core.Entities;
+public class Circuit : EntityBase, IAggregateRoot
+{
+   public Country Country { get; set; } = new Country("");
+   public int CountryId { get; set; }
+
+   public string Name { get; set; }
+
+   public float Length { get; set; }
+
+   public float Laps { get; set; }
+   public int YearFirstGP { get; set; }
+   public int MillisecondsLapRecord { get; set; }
+   public int DriverLapRecord { get; set; }
+   public int YearLapRecord { get; set; }
+
+
+   public Circuit(Country country, string name, float length, float laps, int yearFirstGP, int millisecondsLapRecord, int driverLapRecord, int yearLapRecord)
+   {
+      Country = country;
+      Name = name;
+      Length = length;
+      Laps = laps;
+      YearFirstGP = yearFirstGP;
+      MillisecondsLapRecord = millisecondsLapRecord;
+      DriverLapRecord = driverLapRecord;
+      YearLapRecord = yearLapRecord;
+   }
+   
+   public Circuit(int countryId, string name, float length, float laps, int yearFirstGP, int millisecondsLapRecord, int driverLapRecord, int yearLapRecord)
+   {
+      CountryId = countryId;
+      Name = name;
+      Length = length;
+      Laps = laps;
+      YearFirstGP = yearFirstGP;
+      MillisecondsLapRecord = millisecondsLapRecord;
+      DriverLapRecord = driverLapRecord;
+      YearLapRecord = yearLapRecord;
+   }
+}
---
+++ src/TFG.RulesPenaltiesF1.Core/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using TFG.RulesPenaltiesF1.Core.Interfaces;
+
+namespace TFG.RulesPenaltiesF1.Core.Entities;
+public class Country : EntityBase, IAggregateRoot
+{
+   public string Name { get; set; }
+
+   public Country(string name)
+   {
+      Name = name;
+   }
+}
